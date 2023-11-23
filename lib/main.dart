15024e9ee9404ff3d753148eb694073cc33a60c0
dylanroache70 <dylanroache7@gmail.com>
2023-11-22 22:25:52 -0500

    design layout for home page

--- lib/main.dart
+++ lib/main.dart
@@ -22,51 +22,110 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  child: const Text('Appointments'),
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/appointment');
-                  },
-                ),
-                const SizedBox(width: 20),
-                ElevatedButton(
-                  child: const Text('Contacts'),
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/contact');
-                  },
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  child: const Text('Recalls'),
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/recall');
-                  },
-                ),
-                const SizedBox(width: 20),
-                ElevatedButton(
-                  child: const Text('Tickets'),
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/ticket');
-                  },
-                ),
-              ],
-            )
-          ],
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 35.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _buildButtonWithImage(
+                    context,
+                    'Appointments',
+                    'assets/appointment_image.jpeg',
+                    '/appointment',
+                  ),
+                  _buildButtonWithImage(
+                    context,
+                    'Contacts',
+                    'assets/contacts_image.png',
+                    '/contact',
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _buildButtonWithImage(
+                    context,
+                    'Recalls',
+                    'assets/recalls_image.png',
+                    '/recall',
+                  ),
+                  _buildButtonWithImage(
+                    context,
+                    'Tickets',
+                    'assets/tickets_image.png',
+                    '/ticket',
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
   }
+
+Widget _buildButtonWithImage(
+  BuildContext context,
+  String buttonText,
+  String imagePath,
+  String route,
+) {
+  return GestureDetector(
+    onTap: () {
+      Navigator.pushNamed(context, route);
+    },
+    child: Container(
+      width: 150,
+      height: 120,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.blue,
+          width: 2.0,
+        ),
+        borderRadius: BorderRadius.circular(20.0),
+        color: Colors.white, 
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 3,
+            blurRadius: 5,
+            offset: Offset(0, 3), 
+          ),
+        ],
+      ),
+      padding: EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            imagePath,
+            width: 60,
+            height: 60,
+          ),
+          const SizedBox(height: 10),
+          Text(
+            buttonText,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+
 }
