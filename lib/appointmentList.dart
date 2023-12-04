de4c43796e4a26efd0d9b017c3260836754ba872
Alenka Tang <112491096+AlenkaTang1@users.noreply.github.com>
2023-12-04 14:46:50 -0500

    Create appointmentList.dart

---
+++ lib/appointmentList.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class Today {
+  final String time;
+  final String name;
+  final String details;
+
+  Today({required this.name, required this.time, required this.details});
+}
+
+class TodayListScreen extends StatefulWidget {
+  const TodayListScreen({super.key});
+
+  @override
+  _TodayListScreenState createState() => _TodayListScreenState();
+}
+
+class _TodayListScreenState extends State<TodayListScreen> {
+  List<Today> todos = [
+    Today(
+        time: '9:00 am',
+        name: 'William John',
+        details: 'Regular B-Service for 2020 GLA'),
+    Today(time: '10:00 am', name: 'Kelly Lee', details: 'collision towed in'),
+    Today(
+        time: '11:00 am',
+        name: 'Amanda Young',
+        details: 'Transmission Service'),
+    Today(time: '12:00 pm', name: 'Tony Patterson', details: 'Battery Serivce'),
+    Today(time: '2:00 pm', name: 'Jackie Gilmuour', details: 'A-Service'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Appointments for Today'),
+      ),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(todos[index].time),
+            onTap: () {
+              Navigator.push(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (context, animation1, animation2) =>
+                      TodayDetailsScreen(today: todos[index]),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class TodayDetailsScreen extends StatelessWidget {
+  final Today today;
+
+  TodayDetailsScreen({required this.today});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(today.time),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Name:',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8.0),
+            Text(today.name),
+            const SizedBox(
+              height: 8.0,
+            ),
+            const Text(
+              'Details:',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8.0),
+            Text(today.details),
+          ],
+        ),
+      ),
+    );
+  }
+}
